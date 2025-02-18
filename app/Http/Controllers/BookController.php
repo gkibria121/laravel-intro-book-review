0ab6281fdd1d00c0a3a019268d6532fe15d0c9bf
gkibria <gkibria121@gmail.com>
2025-02-18 15:42:32 +0600

    fix type hint

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
-use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 
