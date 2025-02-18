d5809761869081746119e5c82500885f2ed9e75f
gkibria <gkibria121@gmail.com>
2025-02-18 10:57:57 +0600

    add migration and resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\ReviewController;
+use App\Models\Book;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', fn() => redirect()->route('books.index'));
+Route::resource('books.reviews', ReviewController::class)->only(['create', 'update']);
+Route::resource('/books', BookController::class);
 
 
 Route::fallback(function () {
